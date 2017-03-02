67f7a58f58cc27615cd9e007926793afd2b5d12a
voxfelix <voxfelix@gmail.com>
2017-03-01 20:48:39 -0500

    Update data.js

--- www/js/data.js
+++ www/js/data.js
@@ -1,23 +1,27 @@
 var dataObj = [
-  { name: "Westchester Community Mental Health",
-    phone: "9145551234",
-    location: "550 North St, White Plains, NY 10605",
-    description: "The Department of Community Mental Health plans, oversees and coordinates services for individuals - and their families - with mental illness, developmental disabilities and substance abuse disorders. ",
+  { name: "Helpline Center",
+    phone: "211",
+    description: "We are here for you. All day, every day. At the Helpline Center, we believe everyone deserves help when they need simple answers, community connections or hope during difficult times.",
     category: "1" },
-  { name: "Community Mental Health",
+  { name: "White Plains Hospital: Emergency Room",
     phone: "9145551234",
-    location: "550 North St, White Plains, NY 10605",
-    description: "The Department of Community Mental Health plans, oversees and coordinates services for individuals - and their families - with mental illness, developmental disabilities and substance abuse disorders. ",
+    description: "When it comes to emergency treatment of a heart attack patient, quick intervention is necessary to prevent permanent damage to heart muscle and function.",
+    category: "2" },
+  { name: "Peapod",
+    phone: "9147470519",
+    //web: https://www.peapod.com/shop/index.jhtml#!/
+    description: "Monday-Friday: 8AM - 11PM ET. Saturday - Sunday: 8AM - 4:30PM ET",
     category: "3" },
-  { name: "Test",
+  { name: "Westchester Transportation",
     phone: "9145551234",
-    location: "550 North St, White Plains, NY 10605",
-    description: "The Department of Community Mental Health plans, oversees and coordinates services for individuals - and their families - with mental illness, developmental disabilities and substance abuse disorders. ",
-    category: "3" },
-  { name: "Test2",
-    phone: "9145551234",
-    location: "550 North St, White Plains, NY 10605",
-    description: "The Department of Community Mental Health plans, oversees and coordinates services for individuals - and their families - with mental illness, developmental disabilities and substance abuse disorders. ",
-    category: "3" }
-
+    //web: http://transportation.westchestergov.com/senior-and-disabled-services
+    //location:
+    description: "Seniors and the disabled can stay independent while leaving the driving to us.",
+    category: "4" },
+  { name: "Active Older Adults",
+    postalAddress: "65 Mitchell Place White Plains NY",
+    phone: "9144221423",
+    //web: http://www.cityofwhiteplains.com/index.aspx?NID=362,
+    description: "Available 8:30a, to 4:30pm, Monday to Friday",
+    category: "5" }
 ]
